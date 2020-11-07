c6d134eb36315bf68a931024e5e6538358639ccd
Ben West <Xodarap@users.noreply.github.com>
2020-11-07 00:06:43 -0800

    instructions

---
+++ components/instructions.js
@@ -0,0 +1,19 @@
+import Typography from '@material-ui/core/Typography';
+import { Container } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        'paddingTop': theme.spacing(5)
+    },
+}));
+
+export default function Instructions() {
+    const classes = useStyles();
+    return (
+    <Container className={classes.root}>
+        <Typography component="p" variant="body1" align="center" color="textPrimary" gutterBottom>
+            Upload a picture of yourself in a neutral position, facing straight towards the camera for best results
+        </Typography>
+    </Container>)
+}
